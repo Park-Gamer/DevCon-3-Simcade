2344f6e7fc3f5722966868d3085972235cab8e09
Park <cjparkerdj@gmail.com>
2024-11-21 12:48:26 -0500

    Wrote out player input script with double jump and rotation

--- DevCon #3 Simcade/Assets/Scripts/PlayerMovement.cs
+++ DevCon #3 Simcade/Assets/Scripts/PlayerMovement.cs
@@ -4,138 +4,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField, Range(0f, 100f)]
-    float maxSpeed = 10f;
+    Rigidbody rb;
+    [SerializeField, Range(0, 50)] float moveSpeed = 10f;
+    [SerializeField, Range(0, 25)] float jumpSpeed = 5f;
 
-    [SerializeField, Range(0f, 100f)]
-    float maxAcceleration = 10f, maxAirAcceleration = 1f;
+    [SerializeField, Range(0, 5)] int maxAirJumps = 0; // The maximum number of air jumps
+    public int jumpPhase = 0; // Tracks the current phase of jumping
 
-    [SerializeField, Range(0f, 10f)]
-    float jumpHeight = 2f;
-
-    [SerializeField, Range(0, 5)]
-    int maxAirJumps = 0;
-
-    [SerializeField, Range(0f, 90f)]
-    float maxGroundAngle = 25f;
-
-    float minGroundDotProduct;
-
-    int jumpPhase;
-
-    Vector3 velocity;
-    Vector3 desiredVelocity;
-    Vector3 contactNormal;
-
-    bool desiredJump;
+    [SerializeField] float distanceToGround;
+    public LayerMask groundLayer;
     bool onGround;
 
-    Rigidbody body;
-
-    void Awake()
+    private void Start()
     {
-        body = GetComponent<Rigidbody>();
-        minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        Vector2 playerInput;
-        playerInput.x = Input.GetAxis("Horizontal");
-        playerInput.y = Input.GetAxis("Vertical");
-        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        float inputX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        float inputY = Input.GetAxisRaw("Vertical") * moveSpeed;
 
-        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        rb.velocity = new Vector3(inputX, rb.velocity.y, inputY);
 
-        desiredJump |= Input.GetButtonDown("Jump");
-    }
-
-    private void FixedUpdate()
-    {
-        UpdateState();
-        AdjustVelocity();
-
-        if (desiredJump)
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, distanceToGround, groundLayer))
         {
-            desiredJump = false;
-            Jump();
-        }
-
-        body.velocity = velocity;
-
-        onGround = false;
-    }
-
-    void Jump()
-    {
-        if (onGround || jumpPhase < maxAirJumps)
-        {
-            jumpPhase += 1;
-            float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
-            float alignedSpeed = Vector3.Dot(velocity, contactNormal);
-            if (alignedSpeed > 0f)
-            {
-                jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
-            }
-            velocity += contactNormal * jumpSpeed;
-        }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        EvaluateCollision(collision);
-    }
-
-    void OnCollisionStay(Collision collision)
-    {
-        EvaluateCollision(collision);
-    }
-
-    void EvaluateCollision(Collision collision) 
-    {
-        for (int i = 0; i < collision.contactCount; i++)
-        {
-            Vector3 normal = collision.GetContact(i).normal;
-            if (normal.y >= minGroundDotProduct)
-            {
-                onGround = true;
-                contactNormal = normal;
-            }
-        }
-    }
-
-    void UpdateState()
-    {
-        velocity = body.velocity;
-        if (onGround)
-        {
+            Debug.Log("Ground detected");
             jumpPhase = 0;
+            onGround = true;
         }
         else
         {
-            contactNormal = Vector3.up;
+            onGround = false;
         }
-    }
 
-    Vector3 ProjectOnContactPlane(Vector3 vector)
-    {
-        return vector - contactNormal * Vector3.Dot(vector, contactNormal);
-    }
+        if (Input.GetKeyDown(KeyCode.Space) && onGround || Input.GetKeyDown(KeyCode.Space) && jumpPhase < maxAirJumps)
+        {
+            jumpPhase += 1; // Tracks air jumps
+            rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.z);
+        }
 
-    void AdjustVelocity()
-    {
-        Vector3 xAxis = ProjectOnContactPlane(Vector3.right).normalized;
-        Vector3 zAxis = ProjectOnContactPlane(Vector3.forward).normalized;
-
-        float currentX = Vector3.Dot(velocity, xAxis);
-        float currentZ = Vector3.Dot(velocity, zAxis);
-
-        float acceleration = onGround ? maxAcceleration : maxAirAcceleration;
-        float maxSpeedChange = acceleration * Time.deltaTime;
-
-        float newX = Mathf.MoveTowards(currentX, desiredVelocity.x, maxSpeedChange);
-        float newZ = Mathf.MoveTowards(currentZ, desiredVelocity.z, maxSpeedChange);
-
-        velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
+        transform.forward = new Vector3(rb.velocity.x, 0, rb.velocity.z);
     }
 }
