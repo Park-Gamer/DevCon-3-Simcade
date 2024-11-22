0f3b5f8c800531f95a4a334a6a7d4e67bd4fb0fb
Park <cjparkerdj@gmail.com>
2024-11-22 12:56:27 -0500

    Stamina bar faces towards camera

--- DevCon #3 Simcade/Assets/Scripts/StaminaBar.cs
+++ DevCon #3 Simcade/Assets/Scripts/StaminaBar.cs
@@ -6,15 +6,22 @@ using UnityEngine.UI;
 public class StaminaBar : MonoBehaviour
 {
     public Slider slider;
-
+    private Camera cam;
     public void SetMaxStamina(float stamina)
     {
         slider.maxValue = stamina;
         slider.value = stamina;
+
+        cam = Camera.main;
     }
 
     public void SetStamina(float stamina)
     {
         slider.value = stamina;
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - cam.transform.position);
     }
 }
